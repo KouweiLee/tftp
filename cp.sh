53ccf22f2e809409188e2067e39d9e5ab16aa0ca
KouweiLee <15035660024@163.com>
2024-06-07 16:27:29 +0800

    update

--- cp.sh
+++ cp.sh
@@ -1,2 +1,5 @@
+./compile.sh
+sudo mount /dev/sdb1 /mnt
 sudo cp linux2.dtb /mnt/home/arm64
-sudo cp Image /mnt/home/arm64
+sudo umount /mnt
+sudo umount /dev/sdb1
--- ip.sh
+++ ip.sh
@@ -1,1 +1,1 @@
-sudo ip addr add 192.169.137.2/24 dev enxc8a36202cedb
+sudo ip addr add 192.169.137.2/24 dev enx00e04c680134
